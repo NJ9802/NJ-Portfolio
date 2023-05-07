e29c187dd500cc663608064cfe5a61683560571b
NJ9802 <nelsonjavier9802@gmail.com>
2023-05-06 21:46:41 -0400

    Add revalidation time for data fetched

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,14 @@ import "./globals.css";
 
 export const metadata = {
   title: "NJ Portfolio",
-  description: "Look my incredibles Projects!!",
+  description: `Discover the cutting-edge software engineering portfolio of a dedicated professional,
+     showcasing a diverse range of projects and technical skills. Explore our creative solutions to complex problems,
+      demonstrating expertise in coding languages, debugging, project management, and leadership. 
+      Dive into an engaging and interactive experience that highlights the value of quality over quantity,
+       and reveals a unique approach to software development.`,
 };
+
+export const revalidate = 3600;
 
 export default async function RootLayout({
   children,
