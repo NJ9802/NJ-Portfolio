b882394c2f78f92336d4918817967e03ee1f5c0b
NJ9802 <nelsonjavier9802@gmail.com>
2023-05-06 21:41:44 -0400

    Change skills animation

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -11,12 +11,15 @@ export default function Skill({ skill, directionUp }: Props) {
   return (
     <div className="group flex cursor-pointer">
       <motion.img
-        initial={{ y: directionUp ? 60 : -60, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
+        initial={{ x: directionUp ? 30 : -30, opacity: 0 }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
         transition={{ duration: 1 }}
         viewport={{ once: true }}
         className="rounded-full border border-gray-500 object-cover p-2 w-16 h-16 md:w-18 md:h-18 xl:w-18 xl:h-18
-        group-hover:grayscale transition duration-300 ease-in-out]"
+        group-hover:grayscale transition duration-300 ease-in-out z-10]"
         src={skill.logo}
         alt={skill.title}
         loading="lazy"
